bc988338cd10039b82ea47a98df82a2062061c8d
Benek <beniamin.czaplicki@gmail.com>
2017-05-14 21:02:57 +0200

    recommit

--- helloworlddecoupled/src/main/java/org/qbit/examples/spring/introduction/HelloWorldDecoupledWithFactory.java
+++ helloworlddecoupled/src/main/java/org/qbit/examples/spring/introduction/HelloWorldDecoupledWithFactory.java
@@ -4,4 +4,10 @@ package org.qbit.examples.spring.introduction;
  * Created by Benek on 14.05.2017.
  */
 public class HelloWorldDecoupledWithFactory {
+    public static void main(String[] args) {
+        MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
+        MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
+        mr.setMessageProvider(mp);
+        mr.render();
+    }
 }
