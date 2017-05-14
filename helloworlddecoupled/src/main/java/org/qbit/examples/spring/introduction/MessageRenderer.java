85518eb015a5c7d1392976887682c96c838d88a3
Benek <beniamin.czaplicki@gmail.com>
2017-05-14 15:09:23 +0200

    add message randerer

--- helloworlddecoupled/src/main/java/org/qbit/examples/spring/introduction/MessageRenderer.java
+++ helloworlddecoupled/src/main/java/org/qbit/examples/spring/introduction/MessageRenderer.java
@@ -4,4 +4,7 @@ package org.qbit.examples.spring.introduction;
  * Created by Benek on 14.05.2017.
  */
 public interface MessageRenderer {
+    void render();
+    void setMessageProvider(MessageProvider provider);
+    MessageProvider getMessageProvider();
 }
